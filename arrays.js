84cdd7e4c5efa3c9935c1dd761450bfe911fc145
yossi katzman <issoyk@gmail.com>
2018-02-19 22:30:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,9 +19,9 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
-function 
-
-
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift()
+}
 
 
 
